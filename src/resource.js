2a0ededed68d4ee5295445fd46c12914916d8dac
OlhaLesk <olhalesk@gmail.com>
2017-03-31 23:21:45 +0300

    Final version

---
+++ src/resource.js
@@ -0,0 +1,10 @@
+var res = {
+    Background_png : "res/Background.png",
+    Coin_png       : "res/Coin.png"
+};
+
+// it stores the assets to preload
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
