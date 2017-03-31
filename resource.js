d5400bf85f795ef17c076e5d3107e378d889cf80
OlhaLesk <olhalesk@gmail.com>
2017-03-31 19:39:26 +0300

    One class for all crops

--- resource.js
+++ resource.js
@@ -1,15 +1,14 @@
 var res = {
-    BackgroundTileMap : "res/backgroundTileMap.tmx",
-    Background_png    : "res/Background.png",
-    Coin_png          : "res/Coin.png",
-    Wheat_01_png      : "res/Seed01_01.png",
-    Wheat_02_png      : "res/Seed01_02.png",
-    Strawberry_01_png : "res/Seed02_01.png",
-    Strawberry_02_png : "res/Seed02_02.png",
-    Carrot_01_png     : "res/Seed03_01.png",
-    Carrot_02_png     : "res/Seed03_02.png",
-    Cabbage_01_png    : "res/Seed04_01.png",
-    Cabbage_02_png    : "res/Seed04_02.png"
+    Background_png : "res/Background.png",
+    Coin_png       : "res/Coin.png",
+    tile_0_png     : "res/Seed01_01.png",
+    tile_1_png     : "res/Seed01_02.png",
+    tile_2_png     : "res/Seed02_01.png",
+    tile_3_png     : "res/Seed02_02.png",
+    tile_4_png     : "res/Seed03_01.png",
+    tile_5_png     : "res/Seed03_02.png",
+    tile_6_png     : "res/Seed04_01.png",
+    tile_7_png     : "res/Seed04_02.png"
 };
 
 // it stores the assets to preload
